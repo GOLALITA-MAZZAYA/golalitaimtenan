fd98b325c5ba86822d2d4cfae8f5f6421e9dd72c
Sayedptb123 <sayedptb@gmail.com>
2026-02-02 20:25:10 +0530

    code cleanup

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
 export const HEADER_HEIGHT = 80;
-
 export const MUMAYIZATEMAIL = "mumayizat@golalita.com";
+export const BASE_DOMAIN = "golalita.com";
+export const BASE_URL = `www.${BASE_DOMAIN}`;
+export const SUPPORT_EMAIL = "support@golalita.com";
+export const ORG_ID = 155723;
+export const ORG_CODE = "qcb";
