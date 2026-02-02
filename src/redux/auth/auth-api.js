561daa1eff396a5d51dbd172753d71a0ef61259b
Sayedptb123 <sayedptb@gmail.com>
2026-02-02 20:41:01 +0530

    code cleanup

--- src/redux/auth/auth-api.js
+++ src/redux/auth/auth-api.js
@@ -1,6 +1,6 @@
 import instance, { API_BASE_URL } from "../instance";
 import axios from "axios";
-import { ORG_CODE } from "../../constants";
+import { BASE_URL, ORG_CODE } from "../../constants";
 
 const authApi = {
   login: (body) => instance.post("/user/get_token", body),
@@ -11,12 +11,12 @@ const authApi = {
   getUserBanners: (body) => instance.post("/user/dashboard/banner", body),
   contactUs: (body) => instance.post("/loyalty.contact.us/create", body),
   resetPassword: (body) =>
-    instance.post("https://www.golalita.com/mobile/reset_password", body),
+    instance.post(`https://${BASE_URL}/mobile/reset_password`, body),
   getPublicOrganizations: () => instance.post("/user/org/lists/v3", {}),
   checkCode: (body) => instance.post("/code/check/v2", body),
   register: (body) =>
     instance.post(
-      `https://www.golalita.com/organisation/employee/registration/v2/${ORG_CODE}`,
+      `https://${BASE_URL}/organisation/employee/registration/v2/${ORG_CODE}`,
       body
     ),
   sendOTP: (body) => instance.post(`/send/otp/${ORG_CODE}`, body),
@@ -29,7 +29,7 @@ const authApi = {
   validate_code: (body) => instance.post("/user/validate", body),
   checkPhone: (body) => instance.post("/user/phone/check", body),
   getVersion: () =>
-    instance.post(`https://www.golalita.com/mobile/version`, {}),
+    instance.post(`https://${BASE_URL}/mobile/version`, {}),
 
   getAppStatus: () =>
     axios.get("https://keepcalmlabs.com/wp-json/wp/v2/apps/401"),
