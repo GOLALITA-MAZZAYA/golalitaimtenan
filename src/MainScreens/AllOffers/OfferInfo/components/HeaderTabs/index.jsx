f7192f3161f17e999aa2bcfc4a1b3801aa4a28f2
Sayedptb123 <sayedptb@gmail.com>
2026-02-10 15:01:10 +0530

    offer and info button hided in offer details screen

--- src/MainScreens/AllOffers/OfferInfo/components/HeaderTabs/index.jsx
+++ src/MainScreens/AllOffers/OfferInfo/components/HeaderTabs/index.jsx
@@ -1,21 +1,21 @@
 import i18next from "i18next";
-import {ScrollView, StyleSheet, TouchableOpacity, View} from "react-native";
-import {colors} from "../../../../../components/colors";
-import {useTheme} from "../../../../../components/ThemeProvider";
-import {TypographyText} from "../../../../../components/Typography";
-import {OFFER_TAB_CONSTANTS} from "../../config";
+import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import { colors } from "../../../../../components/colors";
+import { useTheme } from "../../../../../components/ThemeProvider";
+import { TypographyText } from "../../../../../components/Typography";
+import { OFFER_TAB_CONSTANTS } from "../../config";
 
 
 const getTabs = () => {
- const tabs = [
-    {
-      key: OFFER_TAB_CONSTANTS.OFFERS,
-      label: i18next.t('Offer.offers'),
-    },
-    {
-      key: OFFER_TAB_CONSTANTS.INFO,
-      label: i18next.t('Offer.info'),
-    },
+  const tabs = [
+    // {
+    //   key: OFFER_TAB_CONSTANTS.OFFERS,
+    //   label: i18next.t('Offer.offers'),
+    // },
+    // {
+    //   key: OFFER_TAB_CONSTANTS.INFO,
+    //   label: i18next.t('Offer.info'),
+    // },
   ];
 
 
@@ -24,51 +24,51 @@ const getTabs = () => {
 };
 
 
-const HeaderTabs = ({setActiveTab, activeTab}) => {
-    const {isDark} = useTheme();
-    const tabs = getTabs();
+const HeaderTabs = ({ setActiveTab, activeTab }) => {
+  const { isDark } = useTheme();
+  const tabs = getTabs();
 
-    return (
-      <View style={{
-          flex: 1,
-          backgroundColor: isDark ? colors.navyBlue : '#fff',
-          paddingHorizontal: 20,
-          paddingBottom: 10,
-          marginTop: 15
-      }}>
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: isDark ? colors.navyBlue : '#fff',
+      paddingHorizontal: 20,
+      paddingBottom: 10,
+      marginTop: 15
+    }}>
       <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          bounces={false}
-        >
-          {tabs.map(item => {
-            const color = isDark ? colors.mainDarkMode : colors.darkBlue;
-            const activeBorder = isDark ? colors.mainDarkMode : colors.darkBlue;
-            const passiveBorder = isDark ? colors.borderGrey : colors.lightGrey;
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+      >
+        {tabs.map(item => {
+          const color = isDark ? colors.mainDarkMode : colors.darkBlue;
+          const activeBorder = isDark ? colors.mainDarkMode : colors.darkBlue;
+          const passiveBorder = isDark ? colors.borderGrey : colors.lightGrey;
 
-            return (
-              <TouchableOpacity
-                style={[
-                  styles.tab,
-                  {
-                    borderColor:
-                      item.key === activeTab ? activeBorder : passiveBorder,
-                  },
-                ]}
-                onPress={() => setActiveTab(item.key)}
-              >
-                <TypographyText
-                  textColor={color}
-                  size={15}
-                  style={{ fontWeight: '700' }}
-                  title={item.label}
-                />
-              </TouchableOpacity>
-            );
-          })}
-        </ScrollView>
-        </View>
-    )
+          return (
+            <TouchableOpacity
+              style={[
+                styles.tab,
+                {
+                  borderColor:
+                    item.key === activeTab ? activeBorder : passiveBorder,
+                },
+              ]}
+              onPress={() => setActiveTab(item.key)}
+            >
+              <TypographyText
+                textColor={color}
+                size={15}
+                style={{ fontWeight: '700' }}
+                title={item.label}
+              />
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
