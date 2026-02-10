c1b4f3e4a7eb3ec7cbb74807da0102052a97de97
Sayedptb123 <sayedptb@gmail.com>
2026-02-10 17:43:15 +0530

    wallet api updated

--- src/api/wallet.js
+++ src/api/wallet.js
@@ -1,16 +1,17 @@
 import axios from "axios";
+import { Platform } from 'react-native';
 
 export const getBase64PkpassFile = async (data) => {
   const res = await axios.post(
-    "https://golalitatwffer.com/api/go/auth/pass",
+    "https://golalitatwffer.com/api/go/auth/pass/qcb",
+    //"http://10.59.1.57:9001/api/go/auth/pass/qcb",
     data
   );
+  const isAndroid = data.device_type === 'android';
+  const walletData = isAndroid ? res.data?.payloadAndroid : res.data?.payload;
 
-  const pkpassLink = res.data?.payload;
-
-  if (!pkpassLink) {
-    throw new Error();
+  if (!walletData) {
+    throw new Error(`No ${isAndroid ? 'payloadAndroid' : 'payload'} in API response`);
   }
-
-  return pkpassLink;
+  return walletData;
 };
