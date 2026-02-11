1bc235125ad1d5465114fbf0b38462494397a0a7
Sayedptb123 <sayedptb@gmail.com>
2026-02-11 12:46:12 +0530

    eye aligment in Input field fixed

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,26 +1,24 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   View,
   StyleSheet,
   TextInput,
   TouchableOpacity,
   Platform,
-} from "react-native";
-import { colors } from "../colors";
-import { LUSAIL_REGULAR } from "../../redux/types";
-import { mainStyles } from "../../styles/mainStyles";
-import { TypographyText } from "../Typography";
-import EyeSvg from "../../assets/view.svg";
-import { useTheme } from "../ThemeProvider";
-import FlagModal from "../FlagModal/FlagModal";
-import { getTextAlign, isRTL } from "../../../utils";
-import { useTranslation } from "react-i18next";
-import { sized } from "../../Svg";
+} from 'react-native';
+import { colors } from '../colors';
+import { LUSAIL_REGULAR } from '../../redux/types';
+import { mainStyles } from '../../styles/mainStyles';
+import { TypographyText } from '../Typography';
+import EyeSvg from '../../assets/view.svg';
+import { useTheme } from '../ThemeProvider';
+import FlagModal from '../FlagModal/FlagModal';
+import { getTextAlign, isRTL } from '../../../utils';
+import { useTranslation } from 'react-i18next';
+import { sized } from '../../Svg';
 
 const Input = ({
   label,
-  verified,
-  onPressNotVerified,
   wrapperStyle,
   style,
   height,
@@ -29,35 +27,34 @@ const Input = ({
   errorMarginTop,
   onPress,
   disableInputRtl,
-  error,
+  labelColor,
   ...props
 }) => {
   const { t } = useTranslation();
   const [isShowPassword, setIsShowPassword] = useState(
-    props.secureTextEntry ?? false
+    props.secureTextEntry ?? false,
   );
-
   const { isDark } = useTheme();
   const isRtl = !disableInputRtl && isRTL();
   style = {
     ...style,
     backgroundColor: isDark ? colors.darkModeBackground : colors.white,
-    color: isDark ? colors.white : colors.darkBlue,
+    color: isDark ? colors.darkGrey : colors.darkBlue,
   };
 
-  const EyeIcon = sized(EyeSvg, 23, 20, "#999CAD");
+  const EyeIcon = sized(EyeSvg, 23, 20, '#999CAD');
   const EyeActiveIcon = sized(
     EyeSvg,
     23,
     20,
-    isDark ? colors.mainDarkMode : colors.darkBlue
+    isDark ? colors.mainDarkMode : colors.darkBlue,
   );
 
   const isShowSecurityIcon = () => {
-    if (Platform.OS === "android") {
+    if (Platform.OS === 'android') {
       const { secureTextEntry, keyboardType } = props;
 
-      if (keyboardType === "visible-password" && secureTextEntry) {
+      if (keyboardType === 'visible-password' && secureTextEntry) {
         return false;
       }
 
@@ -67,6 +64,17 @@ const Input = ({
     return props.secureTextEntry;
   };
 
+  const getLabelColor = () => {
+    if (props.error) {
+      return '#FF406E';
+    }
+
+    if (labelColor) {
+      return labelColor;
+    }
+
+    return isDark ? colors.darkGrey : colors.darkBlue;
+  };
 
   return (
     <TouchableOpacity
@@ -76,72 +84,37 @@ const Input = ({
       }}
       style={[styles.wrapper, wrapperStyle]}
     >
-      <View
+      <TypographyText
+        title={
+          label === t('ContactUs.mobileNumber') ||
+            label === t('Login.password') ||
+            label === t('Profile.confirmPassword')
+            ? ''
+            : label
+        }
+        textColor={getLabelColor()}
+        size={14}
+        font={LUSAIL_REGULAR}
         style={{
-          flexDirection: isRTL() ? "row-reverse" : "row",
-          justifyContent: "space-between",
-          flex: 1,
+          marginBottom: 3,
+          width: '100%',
+          textAlign: isRTL() ? 'right' : 'left',
+          fontWeight: '700',
         }}
-      >
-        <TypographyText
-          title={label}
-          textColor={
-            props.error ? "#FF406E" : isDark ? colors.darkGrey : colors.darkBlue
-          }
-          size={14}
-          font={LUSAIL_REGULAR}
-          style={{
-            marginBottom: 3,
-            // width: "100%",
-            textAlign: isRTL() ? "right" : "left",
-            fontWeight: "700",
-          }}
-        />
-
-        {verified == "verified" && (
-          <TypographyText
-            title={t("Login.verified")}
-            textColor={props.error ? "#FF406E" : "green"}
-            size={11}
-            font={LUSAIL_REGULAR}
-            style={{
-              marginHorizontal: 3,
-              //width: "100%",
-              textAlign: isRTL() ? "right" : "left",
-              fontWeight: "700",
-            }}
-          />
-        )}
-
-        {verified == "notverified" && (
-          <TouchableOpacity onPress={onPressNotVerified}>
-            <TypographyText
-              title={t("Login.notverified")}
-              textColor={props.error ? "#FF406E" : "red"}
-              size={11}
-              font={LUSAIL_REGULAR}
-              style={{
-                //width: "100%",
-                textAlign: isRTL() ? "right" : "left",
-                fontWeight: "700",
-              }}
-            />
-          </TouchableOpacity>
-        )}
-      </View>
+      />
 
       {isShowSecurityIcon() && (
         <TouchableOpacity
           onPress={() => setIsShowPassword(!isShowPassword)}
           style={[
             {
-              position: "absolute",
+              position: 'absolute',
               top: 32,
               zIndex: 1000,
               width: 30,
               height: 30,
-              alignItems: "center",
-              justifyContent: "center",
+              alignItems: 'center',
+              justifyContent: 'center',
             },
             isRTL() ? { left: 15 } : { right: 15 },
           ]}
@@ -149,25 +122,26 @@ const Input = ({
           {!isShowPassword ? <EyeActiveIcon /> : <EyeIcon />}
         </TouchableOpacity>
       )}
-      <View style={{ position: "relative" }}>
-        {label === t("ContactUs.mobileNumber") ? (
+      <View style={{ position: 'relative' }}>
+        {label === t('ContactUs.mobileNumber') ? (
           <FlagModal
             style={[
               styles.input,
               style,
               props.error && styles.error,
-              { borderColor: isDark ? colors.darkGrey : "transparent" },
+              { borderColor: isDark ? colors.darkGrey : 'transparent' },
             ]}
             ref={innerRef}
             disableRtl={disableInputRtl}
+            placeholderTextColor={isDark ? colors.darkGrey : colors.lightGrey}
             textStyle={[
               style,
               {
                 fontFamily: LUSAIL_REGULAR,
-                fontSize: 18,
+                fontSize: 14,
                 backgroundColor: colors.transparent,
               },
-              isRtl && { textAlign: "right", marginRight: 40 },
+              isRtl && { textAlign: 'right', marginRight: 40 },
               isDark && { color: colors.darkGrey },
             ]}
             {...props}
@@ -178,8 +152,8 @@ const Input = ({
             multiline={height !== undefined}
             ref={innerRef}
             style={[
-              style,
               styles.input,
+              style,
               height && { height },
               props.error && [
                 styles.error,
@@ -187,23 +161,23 @@ const Input = ({
               ],
               icon && { paddingLeft: 45 },
               getTextAlign(),
-              Platform.OS === "android" &&
-                props.secureTextEntry && { elevation: 2 },
-              { borderColor: isDark ? colors.darkGrey : "transparent" },
+              Platform.OS === 'android' &&
+              props.secureTextEntry && { elevation: 2 },
+              { borderColor: isDark ? colors.darkGrey : 'transparent' },
             ]}
             selection={undefined}
             {...props}
             secureTextEntry={isShowPassword}
           />
         )}
-        <View style={{ position: "absolute", left: 15, top: 5 }}>{icon}</View>
-        {error && (
+        <View style={{ position: 'absolute', left: 15, top: 5 }}>{icon}</View>
+        {props.error && (
           <TypographyText
-            title={error}
-            textColor={"#FF406E"}
+            title={props.error}
+            textColor={'#FF406E'}
             size={14}
             font={LUSAIL_REGULAR}
-            style={{ fontWeight: "700", marginTop: errorMarginTop ?? 3 }}
+            style={{ fontWeight: '700', marginTop: errorMarginTop ?? 3 }}
           />
         )}
       </View>
@@ -213,7 +187,7 @@ const Input = ({
 
 const styles = StyleSheet.create({
   wrapper: {
-    position: "relative",
+    position: 'relative',
   },
   input: {
     ...mainStyles.lightShadow,
@@ -224,14 +198,14 @@ const styles = StyleSheet.create({
     fontSize: 18,
     height: 52,
     borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "transparent",
-    textAlign: "left",
+    borderStyle: 'solid',
+    borderColor: 'transparent',
+    textAlign: 'left',
     zIndex: 0,
   },
   error: {
-    backgroundColor: "#FFEBEB",
-    borderColor: "#FF406E",
+    backgroundColor: '#FFEBEB',
+    borderColor: '#FF406E',
   },
 });
 
