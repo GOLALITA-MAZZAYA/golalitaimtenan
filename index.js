af74d9109e2aaff037f1646c5332883afc19fc6c
Sayedptb123 <sayedptb@gmail.com>
2026-02-15 19:56:58 +0530

    code cleanup

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+if (!__DEV__) {
+  console.log = () => { };
+}
+
 AppRegistry.registerHeadlessTask(
   'RNFirebaseBackgroundMessage',
   () => firebaseBackgroundMessage,
