55f1715f9b07eeb5c53884a91c1a5abda923f41b
Sayedptb123 <sayedptb@gmail.com>
2026-02-18 14:55:01 +0530

    basr url updated

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const HEADER_HEIGHT = 80;
 export const MUMAYIZATEMAIL = "mumayizat@golalita.com";
-export const BASE_DOMAIN = "golalita.shop";
-export const BASE_URL = `live.${BASE_DOMAIN}`;
+export const BASE_DOMAIN = "golalita.com";
+export const BASE_URL = `www.${BASE_DOMAIN}`;
 export const SUPPORT_EMAIL = "support@golalita.com";
 export const ORG_ID = 155723;
 export const ORG_CODE = "qcb";
